c819a538c3e57ac26a81fc7e278342be1acae017
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-07-21 14:30:51 -0300

    Primeiro Commit

---
+++ hello-typescript/main.ts
@@ -0,0 +1,22 @@
+var minhavar = 'Minha Variavel';
+
+function minhaFunc(x,y){
+    return x + y;
+}
+
+//ES 6 e ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+    return valor * 2;
+})
+
+numeros.map(valor => valor * 2);
+
+class Matematica {
+    soma(x , y){
+        return x + y;
+    }
+}
