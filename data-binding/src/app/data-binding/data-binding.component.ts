e632ced9af8080e991eb902437f942d1e38531eb
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-07-21 16:49:54 -0300

    Atualização

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  renan : string = 'Viado & corno';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com.br/400/200/nature/';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
