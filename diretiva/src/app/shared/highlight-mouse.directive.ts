2ec7adcd00c0da091885e5c67697225148e76daf
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-08-16 11:27:44 -0300

    Usando o HostBinding

--- diretiva/src/app/shared/highlight-mouse.directive.ts
+++ diretiva/src/app/shared/highlight-mouse.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[highlightMouse]'
 })
 export class HighlightMouseDirective {
 
-  constructor() { }
+  private backgroundColor: string;
+
+  @HostListener('mouseenter') onMouseOver() {
+    //this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color','yellow');
+    this.backgroundColor = 'yellow'
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    //this.renderer.setElementStyle(this.elementRef.nativeElement,'background-color','white');
+    this.backgroundColor = 'white';
+  }
+
+  //Quando não tem manipulação da variavel.
+  //@HostBinding('style.backgroundColor') backgroundColor: string;
+
+  //Quando tem manipulação da variavel.
+  @HostBinding('style.backgroundColor') get setColor(){  
+    return this.backgroundColor;
+  }
+
+  constructor(/*private elementRef: ElementRef, private renderer: Renderer*/) { }
 
 }
