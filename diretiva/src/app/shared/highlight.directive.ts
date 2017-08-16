b34cbe031adb8c89ec35718b010e5183a8fef3a1
Jorge Miranda <jorge.miranda@sam.fortaleza.ce.gov.br>
2017-08-16 14:19:54 -0300

    hostingbinding-completo

---
+++ diretiva/src/app/shared/highlight.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective {
+  @Input() defaultColor: string = 'white';
+  @Input('highlight') highlightColor: string = 'yellow';
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.background = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.background = this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') background: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.background = this.defaultColor;    
+  }
+}
--- diretiva/src/app/shared/shared.module.ts
+++ diretiva/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FundoAmareloDirective } from './fundo-amarelo.directive';
 import { HighlightMouseDirective } from './highlight-mouse.directive';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FundoAmareloDirective, HighlightMouseDirective]
+  declarations: [FundoAmareloDirective, HighlightMouseDirective, HighlightDirective]
 })
 export class SharedModule { }
